6b76cda01f459221ca1e34d2c5000ab070fa37e2
Anisul Islam <air.rohit999@gmail.com>
2021-07-15 16:56:05 +0600

    Basic setting of conftest and BaseClass

---
+++ tests/conftest.py
@@ -0,0 +1,22 @@
+import time
+
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture
+def setup(request):
+    # Initializing the Chrome browser
+    driver = webdriver.Chrome()
+
+    # Maximizing the browser window and hitting the target Url
+    driver.maximize_window()
+    driver.get("https://opensource-demo.orangehrmlive.com/index.php/")
+
+    # Assigning local driver to the class driver so that we use this local driver in the desired class also
+    request.cls.driver = driver
+
+    yield
+
+    time.sleep(2)
+    driver.quit()
---
+++ tests/test_leaveBalanceCalculation.py
@@ -0,0 +1,6 @@
+from utilities.baseClass import BaseClass
+
+
+class TestLeaveBalanceCalculation(BaseClass):
+    def test_leave_balance_calculation(self):
+        pass
