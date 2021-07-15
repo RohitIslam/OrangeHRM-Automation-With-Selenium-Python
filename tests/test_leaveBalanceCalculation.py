8ffbe6018e92c29921318a92d8791fc6b751a756
Anisul Islam <air.rohit999@gmail.com>
2021-07-15 18:07:02 +0600

    Reached to Leave types page from Dashboard using mouse hover and actions

--- tests/test_leaveBalanceCalculation.py
+++ tests/test_leaveBalanceCalculation.py
@@ -1,10 +1,16 @@
+from selenium.webdriver import ActionChains
+
+from page_objects.dashboardPage import DashboardPage
 from page_objects.loginPage import LoginPage
 from utilities.baseClass import BaseClass
 
 
 class TestLeaveBalanceCalculation(BaseClass):
     def test_leave_balance_calculation(self):
+        action = ActionChains(self.driver)
+
         loginPage = LoginPage(self.driver)
+        dashboardPage = DashboardPage(self.driver)
 
         # Login as Admin
         admin_username = 'Admin'
@@ -13,3 +19,8 @@ class TestLeaveBalanceCalculation(BaseClass):
         loginPage.getUsernameField().send_keys(admin_username)
         loginPage.getPassField().send_keys(admin_pass)
         loginPage.getLoginButton().click()
+
+        # Creating Leave type
+        action.move_to_element(dashboardPage.getLeaveButton()).perform()
+        action.move_to_element(dashboardPage.getConfigureButton()).perform()
+        action.move_to_element(dashboardPage.getLeaveTypesButton()).click().perform()
